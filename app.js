6946fca55d4a74749bcf004d173ea274c6593ddb
yifan <ykou6@qq.com>
2022-11-18 12:32:53 -0600

    creation of data

---
+++ app.js
@@ -0,0 +1,65 @@
+//creation of data
+let team1 = {
+    team_name: 'Real Madrid',
+    city: 'Madrid',
+    country: 'Spain',
+    top_scorers: ['Ronaldo', 'Benzema', 'Hazard'],
+    worldwide_fans: 798,
+}
+
+let team2 = {
+    team_name: 'Barcelona',
+    city: 'Barcelona',
+    country: 'Spain',
+    top_scorers: ['Messi', 'Suarez', 'Puyol'],
+    worldwide_fans: 738,
+}
+
+let team3 = {
+    team_name: 'Manchester United',
+    city: 'Manchester',
+    country: 'England',
+    top_scorers: ['Cantona', 'Rooney', 'Ronaldo'],
+    worldwide_fans: 755,
+}
+
+let team4 = {
+    team_name: 'Manchester City',
+    city: 'Manchester',
+    country: 'England',
+    top_scorers: ['Sterling', 'Aguero', 'Haaland'],
+    worldwide_fans: 537,
+}
+
+let team5 = {
+    team_name: 'Brazil National Team',
+    city: 'Not applicable',
+    country: 'Brazil',
+    top_scorers: ['Ronaldinho', 'Cafu', 'Bebeto'],
+    worldwide_fans: 950,
+}
+
+let team6 = {
+    team_name: 'Argentina national team',
+    city: 'Not applicable',
+    country: 'Brazil',
+    top_scorers: ['Messi', 'Batistuta', 'Maradona'],
+    worldwide_fans: 888,
+}
+
+let team7 = {
+    team_name: 'Atletico Madrid',
+    city: 'Madrid',
+    country: 'Spain',
+    top_scorers: ['Aragonés', 'Griezmann', 'Torez'],
+    worldwide_fans: 400,
+}
+
+
+// db.collection('teams').add(team1)
+// db.collection('teams').add(team2)
+// db.collection('teams').add(team3)
+// db.collection('teams').add(team4)
+// db.collection('teams').add(team5)
+// db.collection('teams').add(team6)
+// db.collection('teams').add(team7)
